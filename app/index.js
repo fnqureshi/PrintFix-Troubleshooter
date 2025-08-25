ec6d919c3661033c41084f7e7a8ef4362bc6e6cc
Fardan Qureshi <88404573+fnqureshi@users.noreply.github.com>
2025-08-25 18:11:08 +0000

    FEAT: Forge the Living Ark - The complete, navigable cathedral shell

--- app/index.js
+++ app/index.js
@@ -1,14 +1,1 @@
-// app/index.js
-
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import DefectsScreen from '../screens/DefectsScreen';
-
-export default function HomeScreen() {
-  return (
-    <>
-      <DefectsScreen />
-      <StatusBar style="light" />
-    </>
-  );
-}
+import { Redirect } from 'expo-router';export default function Index() {  return <Redirect href="/home" />;}
